910cafed0f1e0ac650e185b21af5e538031a8d86
Kazuki Ota <k_ota28@hotmail.com>
2019-08-30 16:09:04 +0900

    done

---
+++ GrpcClient/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GrpcSample;
+
+namespace GrpcClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void CallGrpcServiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:5001")
+            })
+            {
+                var greetServices = Grpc.Net.Client.GrpcClient.Create<Greeter.GreeterClient>(client);
+                var response = await greetServices.GreetAsync(new GreetRequest
+                {
+                    Name = textBoxName.Text,
+                });
+                MessageBox.Show(response.Message);
+            }
+        }
+    }
+}
